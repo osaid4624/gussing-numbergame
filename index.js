9e3e3688d10be8601905cd6000afad7ca89e5617
Muhammad Usaid <osaid4624@users.noreply.github.com>
2024-03-26 07:54:06 +0500

    new commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const randomnumber = (Math.floor(Math.random()) * 6 + 1);
+const randomnumber = Math.floor(Math.random() * 6 + 1 );
 const answer = await inquirer.prompt([{
         name: "userGusseuingNumber",
         type: "number",
         message: "Plessae Guess a number"
     }]);
 if (answer.userGusseuingNumber === randomnumber) {
-    console.log("congratulation you! right number");
+    console.log("congratulation you! right number.");
 }
 else {
-    console.log("Ohh worng number try again");
+    console.log("Ohh worng number try again.");
 }
