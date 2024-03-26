5190b7be397c5b7d04d0a4e5785885a590d57b0a
Muhammad Usaid <osaid4624@users.noreply.github.com>
2024-03-26 05:29:01 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random()) * 6 + 1;
+const answer = await inquirer.prompt([
+  {
+    name: "userGusseuingNumber",
+    type: "number",
+    message: "Plessae Guess a number",
+  },
+]);
+if (answer.userGusseuingNumber === randomnumber) {
+  console.log("congratulation you! right number");
+} else {
+  console.log("Ohh worng number try again");
+}
