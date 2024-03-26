967089578a2cefaf8a6f608b27f4e40f959f4d41
Muhammad Usaid <osaid4624@users.noreply.github.com>
2024-03-26 05:38:15 +0500

    my first commit

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import inquirer from "inquirer";
-const randomnumber = Math.floor(Math.random()) * 6 + 1;
-const answer = await inquirer.prompt([
-  {
-    name: "userGusseuingNumber",
-    type: "number",
-    message: "Plessae Guess a number",
-  },
-]);
+const randomnumber = (Math.floor(Math.random()) * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "userGusseuingNumber",
+        type: "number",
+        message: "Plessae Guess a number"
+    }]);
 if (answer.userGusseuingNumber === randomnumber) {
-  console.log("congratulation you! right number");
-} else {
-  console.log("Ohh worng number try again");
+    console.log("congratulation you! right number");
+}
+else {
+    console.log("Ohh worng number try again");
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
+#! /usr/bin/env node
+
 import  inquirer from "inquirer"
 
 const randomnumber=(Math.floor(Math.random()) * 6 + 1);
-
-
-
-
 
 const answer = await inquirer.prompt([{
  name:"userGusseuingNumber",
